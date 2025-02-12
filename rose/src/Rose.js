29a1f08c622cfbdd82bf1fe2e58344890cdfe478
Nolan ORourke <ngorourke@gmail.com>
2025-02-11 18:22:25 -0600

    the cube is now rotatable, time to make the rose

--- rose/src/Rose.js
+++ rose/src/Rose.js
@@ -12,19 +12,22 @@ export default function Rose3D()
         scene.background = new THREE.Color(0xfff0f6);
         console.log('scene is created...');
 
-        const camera = new THREE.PerspectiveCamera(75,
-            window.innerWidth / window.innerHeight,0.1,1000);
-        camera.position.z = 5;
-        console.log('camera is created...');
-
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         console.log('renderer is rendered...');
+
+        const camera = new THREE.PerspectiveCamera(75,
+            window.innerWidth / window.innerHeight,1,500);
+        const controls = new OrbitControls( camera, renderer.domElement);
+        camera.position.set(0,0,3);
+        controls.update();
+        camera.lookAt(0,0,0);
+        console.log('camera is created...');
         
         mountRef.current.appendChild(renderer.domElement);
 
         const geometry = new THREE.BoxGeometry(1,1,1); //(width, height, depth)
-        const material = new THREE.MeshBasicMaterial({color : 0x00ff00}); //should be a bright green box
+        const material = new THREE.LineBasicMaterial({color : 0x00ff00}); //should be a bright green box
         console.log('cube is created...');
     
         const cube = new THREE.Mesh(geometry, material);
@@ -32,8 +35,10 @@ export default function Rose3D()
         console.log('cube should be visible...');
         function animate()
         {
-            cube.rotation.x += 1.02;
-            cube.rotation.y += 1.02;
+            // cube.rotation.x += 0.01;
+            // cube.rotation.y += 0.01;
+            requestAnimationFrame(animate);
+            controls.update();
             renderer.render(scene,camera);
         }
         animate();
@@ -46,5 +51,5 @@ export default function Rose3D()
         }
 
     }, []);
-    return <div ref={mountRef}/>
+    return <div ref={mountRef} style={{overflow: "hidden"}}/>
 }
