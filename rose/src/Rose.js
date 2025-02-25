d7ee85d74b2664d766365fe1754e5cfcafa8dd24
Nolan ORourke <ngorourke@gmail.com>
2025-02-24 23:36:03 -0600

    removed stuff, need to start it over

--- rose/src/Rose.js
+++ rose/src/Rose.js
@@ -26,83 +26,11 @@ export default function Rose3D()
         
         mountRef.current.appendChild(renderer.domElement);
 
-        // const geometry = new THREE.BoxGeometry(1,1,1); //(width, height, depth)
-        // const material = new THREE.LineBasicMaterial({color : 0x00ff00}); //should be a bright green box
-        // console.log('cube is created...');
-        // const cube = new THREE.Mesh(geometry, material);
-        // scene.add(cube); //should add the cube to the scene
-        // console.log('cube should be visible...');
-
         const stemGeometry = new THREE.CylinderGeometry(1, 1.5, 100, 700);
         const stemMaterial = new THREE.MeshBasicMaterial({color: 0x299B29, side: THREE.DoubleSide});
         const stem = new THREE.Mesh(stemGeometry, stemMaterial);
         
         scene.add(stem);
-
-        // function generatePetal(angle)
-        // {
-            const x = 0, y = -19;
-            const petalShape = new THREE.Shape();
-            //petalShape.moveTo(x+5, y+5);
-            // petalShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
-            // petalShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
-            // petalShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
-            // petalShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
-            // petalShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
-            // petalShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
-            //petalShape.bezierCurveTo( x, y, x, y, x, y );
-
-            // petalShape.bezierCurveTo( x + 6, y, x + 6, y - 7,x + 6, y - 7 );
-            // petalShape.bezierCurveTo( x + 6, y - 11, x + 3, y - 15.4, x - 5, y - 19 );
-            // petalShape.bezierCurveTo( x - 12, y - 15.4, x - 16, y - 11, x - 16, y - 7 );
-            // petalShape.bezierCurveTo( x - 16, y - 7, x - 16, y, x - 10, y );
-
-            petalShape.bezierCurveTo( x + 6, y + 19, x + 6, y + 12, x + 6, y + 12 );
-            petalShape.bezierCurveTo( x + 6, y + 8, x + 3, y + 4, x - 5, y );
-            petalShape.bezierCurveTo( x - 12, y + 4, x - 16, y + 8, x - 16, y + 12 );
-            petalShape.bezierCurveTo( x - 16, y + 12, x - 16, y + 19, x - 10, y + 19 );
-            //petalShape.bezierCurveTo( x - 7, y, x - 5, y - 5, x + 5, y + 5 );
-
-            const curve = new THREE.CatmullRomCurve3([
-                new THREE.Vector3(0,0,0),
-                new THREE.Vector3(0,0,1),
-                new THREE.Vector3(0.5,0,2),
-                new THREE.Vector3(0.2,0,3),
-            ]);
-            const extrudeSettings = {
-                //depth: 0.1,
-                steps: 1,
-                bevelEnabled: false,
-                //bevelSize: 0.2,
-                //bevelThickness: 0.3,
-                extrudePath: curve
-            };
-            const petalGeometry = new THREE.ExtrudeGeometry(petalShape, extrudeSettings);
-            const petalMaterial = new THREE.MeshBasicMaterial({color: 0xFF003F, side: THREE.DoubleSide, /*vertexColors: true */});
-            const petal = new THREE.Mesh(petalGeometry, petalMaterial);
-            petal.rotation.x = 5;
-            petal.rotation.y = 4.25
-
-            petal.position.set(-10, 63,9);
-            //  const petalGeometry = new THREE.PlaneGeometry(0.5, 1, 32, 32);
-            //  const petalMaterial = new THREE.MeshBasicMaterial({color: 0xFF003F, side: THREE.DoubleSide});
-            //  const petal = new THREE.Mesh(petalGeometry, petalMaterial);
-            scene.add(petal);
-
-        // }
-        const petalAmount =3
-        let yrotat = 8.5, zrotat=0;
-        for (let i = 0; i < petalAmount; i++, zrotat++) {
-            const petal = new THREE.Mesh(petalGeometry, petalMaterial);
-            
-            // Rotate each petal around the stem
-            let angle = (i / petalAmount) * Math.PI * 2; // Spread petals evenly
-            petal.position.set(Math.cos(angle) * 8, 63, Math.sin(angle) * 8);
-            petal.lookAt(0,63,0);
-            //petal.rotation.y = angle;
-            petal.rotation.z += Math.PI;
-            //scene.add(petal);
-        }
         function animate()
         {
             // cube.rotation.x += 0.01;
